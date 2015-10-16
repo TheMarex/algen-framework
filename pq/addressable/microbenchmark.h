882043d5f3e5fee91ba2371ec92cf1d7fc731114
Patrick Niklaus <patrick.niklaus@student.kit.edu>
2015-10-16 21:41:28 +0200

    Fix memory leak in microbenchmark

--- pq/addressable/microbenchmark.h
+++ pq/addressable/microbenchmark.h
@@ -29,6 +29,7 @@ public:
 
     struct heap_data
     {
+        ~heap_data() { delete[] data; }
         std::vector<void*> handles;
         T* data;
     };
@@ -79,18 +80,19 @@ public:
             //std::make_pair(1<<26, 0xCA55E77E)
         };
 
-        common::register_benchmark("push^n modify^n", "p^n m^n",
-            microbenchmark::fill_heap_random<1>,
-            [](PQ &queue, Configuration config, void* ptr) {
-                heap_data* data = static_cast<heap_data*>(ptr);
-                size_t size = config.first;
-                for (size_t i = 0; i < size; ++i) {
-                    queue.modify(data->handles[i], data->data[i]);
-                }
-            }, microbenchmark::clear_data, configs, benchmarks);
+        // Only good as a overhead baseline, no tree structure
+        //common::register_benchmark("push^n modify^n", "p^n m^n",
+        //    microbenchmark::fill_heap_random<1>,
+        //    [](PQ &queue, Configuration config, void* ptr) {
+        //        heap_data* data = static_cast<heap_data*>(ptr);
+        //        size_t size = config.first;
+        //        for (size_t i = 0; i < size; ++i) {
+        //            queue.modify(data->handles[i], data->data[i]);
+        //        }
+        //    }, microbenchmark::clear_data, configs, benchmarks);
 
         common::register_benchmark("modify^n on filled heap", "filled-m^n",
-            microbenchmark::fill_heap_random_and_pop<1, 3>,
+            microbenchmark::fill_heap_random_and_pop<1, 10>,
             [](PQ &queue, Configuration config, void* ptr) {
                 heap_data* data = static_cast<heap_data*>(ptr);
                 size_t size = config.first;
